f844a8d5a3fca0cc5773fe4b45cb5a3f4bee64dd
Daniel Levy <daniellofficial@gmail.com>
2023-11-30 16:23:50 +0200

    Fix useEffect dependencies in BarberIndex.jsx

--- src/pages/BarberIndex.jsx
+++ src/pages/BarberIndex.jsx
@@ -8,21 +8,21 @@ export function BarberIndex() {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
     const navigate = useNavigate()
 
-    // useEffect(() => {
-    //     loadUsers()
-    // }, [])
+    useEffect(() => {
+        loadUsers()
+    }, [])
 
-    // useEffect(() => {
-    //     window.addEventListener('resize', () => {
-    //         setIsMobile(window.innerWidth < 768)
-    //     })
-    //     return () => {
-    //         window.removeEventListener('resize', () => {
-    //             setIsMobile(window.innerWidth < 768)
-    //         })
-    //     }
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            setIsMobile(window.innerWidth < 768)
+        })
+        return () => {
+            window.removeEventListener('resize', () => {
+                setIsMobile(window.innerWidth < 768)
+            })
+        }
 
-    // }, [window.innerWidth])
+    }, [window.innerWidth])
 
 
     const barbers = users.filter(user => user.isBarber)
